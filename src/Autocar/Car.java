fc64bb8c9ca92262a28ad22261331f2937e29dde
didgmlcks99 <heechy06@naver.com>
2021-03-25 15:09:44 +0900

    b4 mac dies

---
+++ src/Autocar/Car.java
@@ -0,0 +1,8 @@
+package Autocar;
+
+public class Car {
+	public Car() {}
+	public void start() {
+		System.out.println("Self Driving Car Control");
+	}
+}
---
+++ src/Autocar/Main.java
@@ -0,0 +1,10 @@
+package Autocar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Car benzGLS = new Car();
+		benzGLS.start();
+	}
+
+}
